a0956749500babfd0364d2f2437b4b2177f99a5f
Darci <hmg0209@gmail.com>
2020-07-21 17:48:12 +0900

    Update (~props)까지 수강, prop-types 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import propTypes from 'prop-types';
+
+const foodILike = [
+  {id: 1, name: "김치", rating: 5}, 
+  {id: 2, name: "삼겹살", rating: 4.3}, 
+  {id: 3, name: "소고기", rating: 1.2}, 
+  {id: 4, name: "양고기", rating: 3}, 
+  {id: 5, name: "버블티", rating: 1},
+];
+
+function Food({ name, rating }) {
+  return (
+  <div>
+    <h3>I like { name }</h3>
+    <p>{ rating }/5.0</p>
+  </div>
+  );
+}
+
+Food.propTypes = {
+  name: propTypes.string.isRequired,
+  rating: propTypes.number.isRequired,
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>안녕!!!</h1>
+      { foodILike.map(dish => <Food 
+        key={ dish.id } 
+        name={ dish.name } 
+        rating={ dish.rating }/>) }
     </div>
   );
 }
